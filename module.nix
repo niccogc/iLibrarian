6c8f7c0f61f82c8b60cac401dae1759d798e05ef
niccogc <nicci@dtu.dk>
2026-02-15 17:49:54 +0100

    padellaa

--- module.nix
+++ module.nix
@@ -101,8 +101,8 @@ in {
         "pm.max_spare_servers" = 20;
         "request_terminate_timeout" = "60s";
         "pm.max_requests" = 500;
-        "slowlog" = "/var/log/php-fpm/i-librarian.slow.log";
-        "request_slowlog_timeout" = "10s";
+        # "slowlog" = "/var/log/php-fpm/i-librarian.slow.log";
+        # "request_slowlog_timeout" = "10s";
       };
       phpOptions = ''
         upload_max_filesize = 200M
